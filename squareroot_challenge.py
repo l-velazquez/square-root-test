dbd748acc75621be9c580f8706ff557c8baa84d1
Luis Fernando Javier Velazquez Sosa <luisfernandojavier.velazquez@upr.edu>
2021-08-17 22:55:32 -0400

    There is still things to work on but added a lot for now

--- squareroot_challenge.py
+++ squareroot_challenge.py
@@ -1,16 +1,43 @@
-
-x = 1
-y = 10000
-count = 1
+"""There is a video that I saw that a person was able to solve big square roots
+like sqrt(25) is 5 right? So the way they find is by adding 2+5 = 7 and that 7
+you subtract it by 2 because its a square root. If it were a cubic root you would
+subtract by 3.
+"""
+x = 1 #starting number
+max = 10000 #the last number to search for
+a = 0
+check = []
+#here is a list of all the square root all the way to
 sq = []
-
-while x < y:
+while a < max:
 	a = x*x
 	sq.append(a)
-	print(a)
 	x+=1
-	count += 1
-	
-print(sq)
+
+
+for i in sq:
+	if i == 1:
+		check.append(1)
+
+	else:
+		x = True
+		while x:
+			sum = 0
+			num = i % 10
+			sum = num
+			num = int(i/10)
+			if num == 0:
+				sum = i - 2
+				check.append(sum)
+				x = False
+			else:
+				sum += num
+				sum-=2
+				check.append(sum)
+
+
+print(check)
+
+print("The amount of square roots from 1 to",max,":",x)
 	
 
